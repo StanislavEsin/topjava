c9b57955784f085fa9412da8b58ea413a747ad25
StanislavEsin <376825@gmail.com>
2018-07-24 12:14:16 +0600

    Queries "SELECT" via "Criteria" in "JpaMealRepositoryImpl"

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -7,8 +7,12 @@ import java.util.List;
 import java.time.LocalDateTime;
 import javax.persistence.PersistenceContext;
 import javax.persistence.EntityManager;
-import org.springframework.dao.support.DataAccessUtils;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
@@ -16,6 +20,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class JpaMealRepositoryImpl implements MealRepository {
     @PersistenceContext
     private EntityManager em;
+
+    @Autowired
+    CriteriaBuilder builder;
 
     @Transactional
     @Override
@@ -51,27 +58,56 @@ public class JpaMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal get(int id, int userId) {
-        List<Meal> meals = em.createNamedQuery(Meal.GET, Meal.class)
-                .setParameter("id", id)
-                .setParameter("userId", userId)
-                .getResultList();
+//        List<Meal> meals = em.createNamedQuery(Meal.GET, Meal.class)
+//                .setParameter("id", id)
+//                .setParameter("userId", userId)
+//                .getResultList();
+//
+//        return DataAccessUtils.singleResult(meals);
 
-        return DataAccessUtils.singleResult(meals);
+        CriteriaQuery<Meal> criteria = builder.createQuery(Meal.class);
+        Root<Meal> m = criteria.from(Meal.class);
+        criteria.select(m);
+
+        Predicate p1 = builder.equal(m.get("id"), id);
+        Predicate p2 = builder.equal(m.get("user").get("id"), userId);
+        criteria.where(p1, p2);
+
+        return em.createQuery(criteria).getResultStream().findFirst().orElse(null);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return em.createNamedQuery(Meal.GET_ALL, Meal.class)
-                .setParameter("userId", userId)
-                .getResultList();
+//        return em.createNamedQuery(Meal.GET_ALL, Meal.class)
+//                .setParameter("userId", userId)
+//                .getResultList();
+
+        CriteriaQuery<Meal> criteria = builder.createQuery(Meal.class);
+        Root<Meal> m = criteria.from(Meal.class);
+        criteria.select(m);
+
+        Predicate p = builder.equal(m.get("user").get("id"), userId);
+        criteria.where(p).orderBy(builder.desc(m.get("dateTime")));
+
+        return em.createQuery(criteria).getResultList();
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return em.createNamedQuery(Meal.GET_BETWEEN, Meal.class)
-                .setParameter("startDate", startDate)
-                .setParameter("endDate", endDate)
-                .setParameter("userId", userId)
-                .getResultList();
+//        return em.createNamedQuery(Meal.GET_BETWEEN, Meal.class)
+//                .setParameter("startDate", startDate)
+//                .setParameter("endDate", endDate)
+//                .setParameter("userId", userId)
+//                .getResultList();
+
+        CriteriaQuery<Meal> criteria = builder.createQuery(Meal.class);
+        Root<Meal> m = criteria.from(Meal.class);
+        criteria.select(m);
+
+        Predicate p1 = builder.between(m.get("dateTime"), startDate, endDate);
+        Predicate p2 = builder.equal(m.get("user").get("id"), userId);
+        criteria.where(p1, p2).orderBy(builder.desc(m.get("dateTime")));
+
+        return em.createQuery(criteria).getResultList();
     }
 }
