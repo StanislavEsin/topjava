a6959cd1325d6022c24ffeb752235382eb538de8
StanislavEsin <376825@gmail.com>
2018-08-10 14:14:23 +0600

    Put insertRoles outside of if / else

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -52,15 +52,15 @@ public class JdbcUserRepositoryImpl implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-            insertRoles(user);
         } else if (namedParameterJdbcTemplate.update(
                 "UPDATE users SET name=:name, email=:email, password=:password, " +
                         "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
             return null;
         } else {
             deleteRoles(user.getId());
-            insertRoles(user);
         }
+
+        insertRoles(user);
 
         return user;
     }
