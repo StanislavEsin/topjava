a8c1d95f2da61ab20e7c67e3e924d284144c47dd
StanislavEsin <376825@gmail.com>
2018-09-02 17:23:25 +0600

    Translate 'meals.jsp' to work on 'ajax'

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.web.meal;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -22,6 +23,12 @@ public class MealAjaxController extends AbstractMealController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<MealWithExceed> getAll() {
         return super.getAll();
+    }
+
+    @Override
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Meal get(@PathVariable("id") int id) {
+        return super.get(id);
     }
 
     @Override
